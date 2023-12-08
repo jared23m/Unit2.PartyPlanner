ff30b5d195c3e586dcff82a77ea03bd83925b1e7
Jared May <jared23m@gmail.com>
2023-12-08 13:21:09 -0600

    situation with guests are rsvps working

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ addPartyForm.addEventListener("submit", addParty);
 // MAIN FUNCTION: gets the parties from the server and renders them onto the page
 async function getAndRender() {
     await getParties();
+    await getGuests();
+    await getRsvps();
     renderParties();
   }
   getAndRender();
@@ -41,6 +43,17 @@ async function getGuests() {
     const response = await fetch(API_URL_Guests);
     const json = await response.json();
     state.guests = json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// gets the rsvps from the server
+async function getRsvps() {
+  try {
+    const response = await fetch(API_URL_Rsvps);
+    const json = await response.json();
+    state.rsvps = json.data;
   } catch (error) {
     console.error(error);
   }
@@ -72,27 +85,51 @@ async function addParty(event) {
 }
 
 // adds new guest based on the input on the form
-async function addGuest(event) {
+async function addGuest(event, partyId) {
     event.preventDefault();
   
     try {
       const response = await fetch(API_URL_Guests, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({  name: addGuestForm.elements["name"].value,
-          email: addGuestForm.elements["email"].value,
-          phone: addGuestForm.elements["phone"].value
+        body: JSON.stringify({  name: event.target[0].value,
+          email: event.target[1].value,
+          phone: event.target[2].value
         }),
       });
       
       if (!response.ok) {
         throw new Error("Failed to create guest");
       }
+
+      addRsvp(state.guests[state.guests.length - 1].id, partyId);
   
       getAndRender();
     } catch (error) {
       console.error(error);
     }
+}
+
+// creates RSVP based on the current IDs
+async function addRsvp(guestId, partyId) {
+
+  try {
+    const response = await fetch(API_URL_Rsvps, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({  guestId: guestId,
+        eventId: partyId
+      }),
+    });
+    
+    if (!response.ok) {
+      throw new Error("Failed to create rsvp");
+    }
+
+    getAndRender();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // deletes party according to which button is pressed
@@ -126,18 +163,50 @@ function renderParties() {
             } else {
                 cleanTime = `${cleanTime} AM`;
             }
+
+            let rsvpCount = 0;
+            state.rsvps.forEach((rsvp) => {
+              if (rsvp.eventId === party.id){
+                rsvpCount++;
+              }
+            });
+
             card.innerHTML = `
             <h2>${party.name}</h2>
             <p>${cleanDate}</p>
             <p>${cleanTime}</p>
             <p>${party.location}</p>
             <p>${party.description}</p>
+            <p>RSVPs: ${rsvpCount}</p>
             `;
 
             const deleteButton = document.createElement("button");
             deleteButton.innerText = "Delete Party";
             card.append(deleteButton);
             deleteButton.addEventListener('click', () => {deleteParty(party.id)});
+
+            const br = document.createElement("p");
+            br.innerText = "";
+            card.append(br);
+
+            const guestForm = document.createElement("form");
+            guestForm.innerHTML = `
+            <label>
+              Guest Name
+              <input type="text" name="name" />
+            </label>
+            <label>
+              Email
+              <input type="email" name="email" />
+            </label>
+            <label>
+              Phone
+              <input type="tel" name="phone" />
+            </label>
+            <button>Add Guest</button>`;
+            card.append(guestForm);
+            guestForm.addEventListener("submit", (event) => {addGuest(event, party.id)});
+
             return card;
         })
         partyList.replaceChildren(...partyCards);
